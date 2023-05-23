984e1d2eb8dce16e6e8ec601c6dc300310386117
lys <yoonseon12@naver.com>
2023-05-23 13:46:05 +0900

    [프로젝트 생성]
    - 최초 프로젝트 생성
    - h2 database 설정

---
+++ src/main/java/com/healthcare/todohealthcare/TodoHealthcareApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.todohealthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoHealthcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoHealthcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthcare/todohealthcare/TodoHealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.todohealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoHealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
